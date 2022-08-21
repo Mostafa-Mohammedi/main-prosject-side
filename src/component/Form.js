99097d4b05706396f4ccd5114067ec257a2d221c
mosti-developer-94 <mosti94@hotmail.com>
2022-08-21 13:48:27 +0200

    added changes for hosting page on github in package json

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,38 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from "react";
 import "../component/form.css";
+import emailjs from "@emailjs/browser";
 
 const Form = () => {
-    const [inputfield, setInputField] = useState("");
+  const form = useRef();
 
-    const emailValue = e => {
-        setInputField(e.target.value);
+  const [inputfield, setInputField] = useState("");
+
+  const emailValue = (e) => {
+    setInputField(e.target.value);
+  };
+
+  const emailCheck = () => {
+    if (inputfield.length > 0) {
+      console.log(inputfield);
+    } else {
+      alert("please enter email");
     }
 
-    const emailCheck = () =>{
-        if(inputfield.length > 0){;
-            console.log(inputfield);
-        } else{
-            alert("please enter email")
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_d1k02dz",
+        "website-template",
+        form.current,
+        "UoEF4-fB-baM4gVRr"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("email has been sent to mostafa.mohammedi.home@gmail.com")
+        },
+        (error) => {
+          console.log(error.text);
         }
+      );
+      setInputField("");
 
-        setInputField("");
-
-    }
-
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-    }
+  };
 
   return (
-    <form className='form-content' onSubmit={submitHandler}>
-        
-        <label >Contact</label>
+    <form className="form-content" onSubmit={submitHandler} ref={form}>
+      <label>Contact</label>
 
-        <input className='form-name' placeholder="Write Email" type="email" value={inputfield} onChange={emailValue}/>
-        <input className='form-button'  type="submit" onClick={emailCheck}/>
+      <input
+        className="form-name"
+        placeholder="Write Email"
+        type="email"
+        name="email"
+        value={inputfield}
+        onChange={emailValue}
+      />
+      <input className="form-button" type="submit" onClick={emailCheck} />
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
